3002157d096a63f97cbb9284cf51385b8700751e
Yehia Helal <mangetsuhozuki287@yahoo.com>
2023-06-19 15:33:28 +0300

    302

--- models/userModel.js
+++ models/userModel.js
@@ -29,7 +29,7 @@ const userSchema = new Schema(
     role: {
       type: String,
       enum: ["user", "admin"],
-      default: "user",
+      default: "admin",
     },
     photo: {
       type: String,
